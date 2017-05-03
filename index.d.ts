d50929fa7c3e4b1e920e040fd3c6dbb3bcde6204
Travis Schettler <travis_schettler@hotmail.com>
2017-05-02 22:57:35 -0500

    Restructuring

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-import * as OData4DataServiceAdapter from "./breeze.dataService.odata4";
-import { ClassRegistry } from "ClassRegistry";
+import * as OData4DataServiceAdapter from "./src/breeze.dataService.odata4";
+import { ClassRegistry } from "./src/ClassRegistry";
 
 export { ClassRegistry, OData4DataServiceAdapter }
