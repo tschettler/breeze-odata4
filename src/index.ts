6cc26cdb787bbb8479cd45c9a6a8410e8cf6431d
Travis Schettler <travis_schettler@hotmail.com>
2017-05-18 15:05:17 -0500

    Renamed odata 4 data service

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { OData4ServiceAdapter } from './breeze.dataService.odata4';
+import { OData4ServiceAdapter } from './breeze-odata4-dataService';
 import { ClassRegistry, ClassRepository, Type } from './class-registry';
 
 export { ClassRegistry, ClassRepository, Type, OData4ServiceAdapter };
