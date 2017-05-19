413071c69f939fb7411e0eb98e3713d28dc933d1
Travis Schettler <travis_schettler@hotmail.com>
2017-05-19 08:54:17 -0500

    Added adapters and decorators to the exports

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,10 @@
 export { OData4DataService } from './breeze-odata4-dataService';
 export { ClassRegistry, ClassRepository, Type } from './class-registry';
 export { MetadataAdapter } from './adapters/metadata-adapter';
+export { AnnotationAdapter } from './adapters/annotation-adapter';
+export { NavigationAdapter } from './adapters/navigation-adapter';
+
 export { AnnotationDecorator } from './decorators/annotation-decorator';
+export { DisplayNameDecorator, ExpressionWithDisplayName } from './decorators/display-name-decorator';
+export { StoreGeneratedPatternDecorator, ExpressionWithStoreGeneratedPattern } from './decorators/store-generated-pattern-decorator';
+export { ValidatorDecorator, ExpressionWithValidators } from './decorators/validator-decorator';
