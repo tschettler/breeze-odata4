3a5dd0fc94e0064ce5a609815b70b0035a7ea2f4
Travis Schettler <travis_schettler@hotmail.com>
2018-11-24 17:11:15 -0600

    Added tests for getActions and getFunctions

--- src/utilities.ts
+++ src/utilities.ts
@@ -2,124 +2,118 @@ import { ComplexType, DataType, DataTypeSymbol, EntityType, IStructuralType, Met
 import { Edm, Edmx, oData } from 'ts-odatajs';
 
 export interface InvokableEntry {
-    config: Edm.Action | Edm.Function;
-    namespace: string;
-    url?: string;
+  config: Edm.Action | Edm.Function;
+  namespace: string;
+  url?: string;
 }
 
 export namespace Utilities {
+  const dataTypeMap: { [key: string]: DataTypeSymbol } = {
+    binary: DataType.Binary,
+    bool: DataType.Boolean,
+    date: DataType.DateTimeOffset,
+    datetimeoffset: DataType.DateTimeOffset,
+    decimal: DataType.Decimal,
+    duration: DataType.Duration,
+    // enumMember?
+    float: DataType.Double,
+    guid: DataType.Guid,
+    int: DataType.Int64,
+    string: DataType.String,
+    single: DataType.Single,
+    timeofday: DataType.TimeOfDay
+  };
 
-    const dataTypeMap: { [key: string]: DataTypeSymbol } = {
-        binary: DataType.Binary,
-        bool: DataType.Boolean,
-        date: DataType.DateTimeOffset,
-        datetimeoffset: DataType.DateTimeOffset,
-        decimal: DataType.Decimal,
-        duration: DataType.Duration,
-        // enumMember?
-        float: DataType.Double,
-        guid: DataType.Guid,
-        int: DataType.Int64,
-        string: DataType.String,
-        single: DataType.Single,
-        timeofday: DataType.TimeOfDay
-    };
+  export function getDataType(key: string): DataTypeSymbol {
+    const dataType = dataTypeMap[key] || dataTypeMap.string;
+    return dataType;
+  }
 
-    export function getDataType(key: string): DataTypeSymbol {
-        const dataType = dataTypeMap[key] || dataTypeMap.string;
-        return dataType;
+  export function getEdmTypeFromTypeName(metadata: Edmx.Edmx, typeName: string): Edm.ComplexType | Edm.EntityType {
+    const bindingTypeName = oData.utils.getCollectionType(typeName) || typeName;
+    const entityType = oData.utils.lookupEntityType(bindingTypeName, metadata);
+    const complexType = oData.utils.lookupComplexType(bindingTypeName, metadata);
+
+    return entityType || complexType;
+  }
+
+  export function adaptComplexType(metadataStore: MetadataStore, complexType: Edm.ComplexType): ComplexType {
+    return <ComplexType>adaptStructuralType(metadataStore, complexType);
+  }
+
+  export function adaptEntityType(metadataStore: MetadataStore, entityType: Edm.EntityType): EntityType {
+    return <EntityType>adaptStructuralType(metadataStore, entityType);
+  }
+
+  export function adaptStructuralType(metadataStore: MetadataStore, structuralType: Edm.ComplexType | Edm.EntityType): IStructuralType {
+    const result = metadataStore.getEntityType(structuralType.name, true);
+
+    return result;
+  }
+
+  export function getActions(metadata: Edmx.Edmx, metadataStore: MetadataStore): InvokableEntry[] {
+    const result = getInvokableEntries(metadata, metadataStore, s => s.action);
+
+    return result;
+  }
+
+  export function getFunctions(metadata: Edmx.Edmx, metadataStore: MetadataStore): InvokableEntry[] {
+    const result = getInvokableEntries(metadata, metadataStore, s => s.function);
+
+    return result;
+  }
+
+  export function getInvokableEntries(
+    metadata: Edmx.Edmx,
+    metadataStore: MetadataStore,
+    accessor: (schema: Edm.Schema) => Edm.Action[] | Edm.Function[]
+  ): InvokableEntry[] {
+    let result: InvokableEntry[] = [];
+
+    metadata.dataServices.schema.forEach(s => {
+      const namespace = s.namespace;
+      const items: any[] = accessor(s);
+      if (!items) {
+        return;
+      }
+
+      const entries = items.map((config: Edm.Action | Edm.Function) => {
+        const entry: InvokableEntry = {
+          config: config,
+          namespace: namespace,
+          url: getInvokableUrl(metadata, metadataStore, config, namespace)
+        };
+
+        return entry;
+      });
+
+      result = result.concat(entries);
+    });
+
+    return result;
+  }
+
+  export function getInvokableUrl(
+    metadata: Edmx.Edmx,
+    metadataStore: MetadataStore,
+    config: Edm.Function | Edm.Action,
+    namespace: string
+  ): string {
+    const isBound = Boolean(config.isBound);
+    let boundPart = '';
+
+    if (isBound) {
+      const bindingParameter = config.parameter[0];
+      const bindingEdmType = getEdmTypeFromTypeName(metadata, bindingParameter.type);
+      const breezeType = adaptEntityType(metadataStore, bindingEdmType);
+
+      if (breezeType) {
+        boundPart = `${breezeType.defaultResourceName}/`;
+      }
     }
 
-    export function getEdmTypeFromTypeName(metadata: Edmx.Edmx, typeName: string): Edm.ComplexType | Edm.EntityType {
-        const bindingTypeName = oData.utils.getCollectionType(typeName) || typeName;
-        const entityType = oData.utils.lookupEntityType(bindingTypeName, metadata);
-        const complexType = oData.utils.lookupComplexType(bindingTypeName, metadata);
+    const url = `${boundPart}${namespace}.${config.name}`;
 
-        return entityType || complexType;
-    }
-
-    export function adaptComplexType(metadataStore: MetadataStore, complexType: Edm.ComplexType): ComplexType {
-        return <ComplexType>adaptStructuralType(metadataStore, complexType);
-    }
-
-    export function adaptEntityType(metadataStore: MetadataStore, entityType: Edm.EntityType): EntityType {
-        return <EntityType>adaptStructuralType(metadataStore, entityType);
-    }
-
-    export function adaptStructuralType(metadataStore: MetadataStore, structuralType: Edm.ComplexType | Edm.EntityType): IStructuralType {
-        const result = metadataStore.getEntityType(structuralType.name, true);
-
-        return result;
-    }
-
-    export function getFunctions(metadata: Edmx.Edmx, metadataStore: MetadataStore): InvokableEntry[] {
-        let result = [];
-
-        metadata.dataServices.schema.forEach(s => {
-            const namespace = s.namespace;
-            if (!s.function) {
-                return;
-            }
-
-            const entries: InvokableEntry[] = s.function.map(c => {
-                const entry: InvokableEntry = {
-                    config: c,
-                    namespace: namespace,
-                    url: getInvokableUrl(metadata, metadataStore, c, namespace)
-                };
-
-                return entry;
-            });
-
-            result = result.concat(entries);
-        });
-
-        return result;
-    }
-
-    export function getActions(metadata: Edmx.Edmx, metadataStore: MetadataStore): InvokableEntry[] {
-        let result = [];
-
-        metadata.dataServices.schema.forEach(s => {
-            const namespace = s.namespace;
-            if (!s.action) {
-                return;
-            }
-
-            const entries: InvokableEntry[] = s.action.map(c => {
-                const entry: InvokableEntry = {
-                    config: c,
-                    namespace: namespace,
-                    url: getInvokableUrl(metadata, metadataStore, c, namespace)
-                };
-
-                return entry;
-            });
-
-            result = result.concat(entries);
-        });
-
-        return result;
-    }
-
-    export function getInvokableUrl(metadata: Edmx.Edmx, metadataStore: MetadataStore,
-        config: Edm.Function | Edm.Action, namespace: string): string {
-
-        const isBound = Boolean(config.isBound);
-        let boundPart = '';
-
-        if (isBound) {
-            const bindingParameter = config.parameter[0];
-            const bindingEdmType = getEdmTypeFromTypeName(metadata, bindingParameter.type);
-            const breezeType = adaptEntityType(metadataStore, bindingEdmType);
-
-            if (breezeType) {
-                boundPart = `${breezeType.defaultResourceName}/`;
-            }
-        }
-
-        const url = `${boundPart}${namespace}.${config.name}`;
-
-        return url;
-    }
+    return url;
+  }
 }
