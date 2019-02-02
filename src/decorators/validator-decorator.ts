3a580fcab88465ae705346d806a36f5734b710b1
Travis Schettler <travis_schettler@hotmail.com>
2019-02-02 11:13:32 -0600

    Added unit tests for ValidatorDecorator.

--- src/decorators/validator-decorator.ts
+++ src/decorators/validator-decorator.ts
@@ -5,38 +5,46 @@ import { Utilities } from '../utilities';
 import { AnnotationDecorator } from './annotation-decorator';
 
 export interface ExpressionWithValidators extends Edm.Base.Annotatable {
-    validators?: any[];
+  validators?: any[];
 }
 
 const ValidatorTerm = 'Validator';
 export class ValidatorDecorator implements AnnotationDecorator {
-    public canDecorate(annotation: Edm.Annotation): boolean {
-        return annotation.term.indexOf(`.${ValidatorTerm}`) > -1;
+  public canDecorate(annotation: Edm.Annotation): boolean {
+    return annotation.term.indexOf(`.${ValidatorTerm}`) > -1;
+  }
+
+  public decorate(expression: ExpressionWithValidators, annotation: Edm.Annotation): void {
+    expression.validators = expression.validators || [];
+
+    const keys = Object.keys(annotation);
+    core.arrayRemoveItem(keys, 'term', false);
+
+    const valueKey = keys[0];
+    const value = annotation[valueKey];
+
+    const nameAndProp = annotation.term.replace(/^.*Validator\.?/, '').split('.');
+    const name = nameAndProp.shift();
+    const prop = nameAndProp.shift();
+
+    if (!name) {
+      return;
     }
 
-    public decorate(expression: ExpressionWithValidators, annotation: Edm.Annotation): void {
-        expression.validators = expression.validators || [];
+    let validator = expression.validators.find((val: { name: string }) => {
+      return val.name === name;
+    });
 
-        const keys = Object.keys(annotation);
-        core.arrayRemoveItem(keys, 'term', false);
-
-        const valueKey = keys[0];
-        const value = annotation[valueKey];
-
-        const nameAndProp = annotation.term.replace(/^.*Validator\./, '').split('.');
-        const name = nameAndProp.shift();
-        const prop = nameAndProp.shift();
-
-        let validator = expression.validators.find((val: { name: string; }) => {
-            return val.name === name;
-        });
-
-        if (!validator) {
-            validator = { name: name };
-            expression.validators.push(validator);
-        }
-
-        const dataType = Utilities.getDataType(valueKey);
-        validator[prop] = dataType.parse(value, 'string');
+    if (!validator) {
+      validator = { name: name };
+      expression.validators.push(validator);
     }
+
+    if (!prop) {
+      return;
+    }
+
+    const dataType = Utilities.getDataType(valueKey);
+    validator[prop] = dataType.parse(value, 'string');
+  }
 }
